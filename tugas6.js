7e1683ae4927028058c9e9876cee9889208f2786
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-09 00:37:17 +0700

    Upload Tugas 6 JS

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+const tampilkanGanjil = () =>{
+    for(let i = 1; i <= 20; i++){
+        if(i % 2 == 0){
+            console.log('Bilangan Genap!');
+        }else{
+            console.log(i);
+        }
+    }
+}
+
+tampilkanGanjil();
